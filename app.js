08e73af918f246febd707c1243e8a336ea8355f3
kira-xc-steep <80065689+kira-xc-steep@users.noreply.github.com>
2021-08-06 18:43:37 +0500

    Update app.js
    убрал ненужные куски кода

--- app.js
+++ app.js
@@ -10,31 +10,24 @@ const person = {
     }
 } // Id person: ZmlsZTovLy9ob21lL3dob25hbWUwMC9QeXRob25Qcm9qZWN0L0NhckdhbWUuemlw
 
-const car = document.getElementById("car") // ElementId: car (player, you, other)
+const car = document.getElementById("car")
 
 document.addEventListener("keydown", function(event) {
     right();
 });
 
 function right () {
-    if (car.classList != "right") {
-        car.classList.add("right")
-    }
-    setTimeout( function() {
-        car.classList.remove("right")
-    }, 300)
+if (car.classList != "right") {
+	car.classList.add("right")
+	}
 }
 
 document.addEventListener("keydown", function(event) {
     left();
 });
 function left () {
-    if (car.classList != "left") {
-        car.classList.add("left")
-    }
-    setTimeout( function() {
-        car.classList.remove("left")
-    }, 300)
+if (car.classList != "left") {
+    car.classList.add("left")
+	}
 }
-
 console.log(person)
